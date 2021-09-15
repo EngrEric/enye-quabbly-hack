3f072a9bb2cfee0c04223d41108cddf44a67852d
SantosBright <itzsantosbright15@gmail.com>
2021-09-15 17:45:38 +0100

    marking page

---
+++ src/dashboard/Marking.js
@@ -0,0 +1,172 @@
+import React from 'react';
+import { Table } from 'antd';
+import Layout from './Layout';
+import 'antd/dist/antd.min.css';
+
+const Marking = () => {
+  const columns = [
+    {
+      title: <span className='font-lato text-sm font-semibold'>Name</span>,
+      dataIndex: 'name',
+      key: 'name',
+      render: name => <span className='font-lato text-sm'>{name}</span>,
+    },
+    {
+      title: <span className='font-lato text-sm font-semibold'>ID</span>,
+      dataIndex: 'id',
+      key: 'id',
+      render: id => <span className='font-lato text-sm'>{id}</span>,
+    },
+    {
+      title: <span className='font-lato text-sm font-semibold'>Ajot Plan</span>,
+      dataIndex: 'plan',
+      key: 'plan',
+      render: plan => <span className='font-lato text-sm'>{plan}</span>,
+    },
+    {
+      title: <span className='font-lato text-sm font-semibold'>Duration</span>,
+      dataIndex: 'duration',
+      key: 'duration',
+      render: duration => <span className='font-lato text-sm'>{duration}</span>,
+    },
+    {
+      title: <span className='font-lato text-sm font-semibold'>Amount</span>,
+      dataIndex: 'amount',
+      key: 'amount',
+      render: amount => <span className='font-lato text-sm'>{amount}</span>,
+    },
+    {
+      title: <span className='font-lato text-sm font-semibold'>Date</span>,
+      dataIndex: 'date',
+      key: 'date',
+      render: date => (
+        <span className='font-lato text-sm'>
+          {new Date(date).getDate()}/{new Date(date).getMonth()}/
+          {new Date(date).getFullYear()}
+        </span>
+      ),
+    },
+    {
+      title: <span className='font-lato text-sm font-semibold'>Status</span>,
+      dataIndex: 'status',
+      key: 'status',
+      render: status => (
+        <span
+          className={`font-lato text-sm capitalize py-1 rounded ${
+            status.toLowerCase() === 'not paid' &&
+            'bg-red-400 bg-opacity-30 px-3 text-red-600 rounded'
+          } ${
+            status.toLowerCase() === 'paid' &&
+            'bg-green-400 bg-opacity-30 px-5 text-green-600 rounded'
+          }`}
+        >
+          {status}
+        </span>
+      ),
+    },
+    {
+      title: <span className='font-lato text-sm font-semibold'>Action</span>,
+      render: record => (
+        <button className='focus:outline-none outline-none cursor-pointer'>
+          <svg
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M12 8C13.1 8 14 7.1 14 6C14 4.9 13.1 4 12 4C10.9 4 10 4.9 10 6C10 7.1 10.9 8 12 8ZM12 10C10.9 10 10 10.9 10 12C10 13.1 10.9 14 12 14C13.1 14 14 13.1 14 12C14 10.9 13.1 10 12 10ZM12 16C10.9 16 10 16.9 10 18C10 19.1 10.9 20 12 20C13.1 20 14 19.1 14 18C14 16.9 13.1 16 12 16Z'
+              fill='#B7BCC2'
+            />
+          </svg>
+        </button>
+      ),
+    },
+  ];
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      id: 32,
+      plan: 'Short Plan',
+      duration: '2 Months',
+      date: new Date(),
+      amount: '₦5000/day',
+      status: 'Paid',
+    },
+    {
+      key: '1',
+      name: 'Mike',
+      id: 32,
+      plan: 'Short Plan',
+      duration: '2 Months',
+      date: new Date(),
+      amount: '₦5000/day',
+      status: 'Paid',
+    },
+    {
+      key: '1',
+      name: 'Mike',
+      id: 32,
+      plan: 'Short Plan',
+      duration: '2 Months',
+      date: new Date(),
+      amount: '₦5000/day',
+      status: 'Paid',
+    },
+    {
+      key: '1',
+      name: 'Mike',
+      id: 32,
+      plan: 'Short Plan',
+      duration: '2 Months',
+      date: new Date(),
+      amount: '₦5000/day',
+      status: 'not Paid',
+    },
+    {
+      key: '1',
+      name: 'Mike',
+      id: 32,
+      plan: 'Short Plan',
+      duration: '2 Months',
+      date: new Date(),
+      amount: '₦5000/day',
+      status: 'Paid',
+    },
+  ];
+
+  return (
+    <Layout>
+      <div className='p-3'>
+        <div className='flex pb-5 pt-2'>
+          <form className='flex-1 relative'>
+            <input
+              type='text'
+              className='text-base py-2 px-5 pl-12 block w-full outline-none focus:outline-none ring-1 rounded-lg ring-gray-400 focus:ring-2 focus:ring-blue-300 duta'
+            />
+            <label
+              htmlFor='search'
+              className='absolute top-0 bottom-0 h-full left-0 px-5 flex items-center text-gray-400'
+            >
+              <i className='fa fa-search'></i>
+            </label>
+            <button className='hidden'>submit</button>
+          </form>
+          <button className='text-primary flex items-center bg-active font-semibold ml-3 px-5 py-2 rounded-lg duration-200 hover:bg-opacity-80 text-sm'>
+            Export
+            <i className='fa fa-sort-down text-primary ml-3'></i>
+          </button>
+          <button className='text-white bg-primary mr-3 px-5 py-2 rounded-lg ml-3 duration-200 hover:bg-opacity-80 text-sm'>
+            <i className='fa fa-plus mr-3'></i>
+            Add Customer
+          </button>
+        </div>
+        <Table dataSource={dataSource} columns={columns} />;
+      </div>
+    </Layout>
+  );
+};
+
+export default Marking;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-import React, {Suspense, useEffect, useRef} from 'react';
+import React, { Suspense, useEffect, useRef } from 'react';
 import { Redirect, Switch } from 'react-router';
 import { Route, BrowserRouter } from 'react-router-dom';
 import {
@@ -10,17 +10,19 @@ import NavBar from '../components/navbar';
 import Landing from '../home/landing';
 import Login from '../home/login';
 import Dashboard from '../dashboard/Index';
+import Marking from '../dashboard/Marking';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <NavBar />
       <Suspense fallback={<h1>Loading</h1>}>
-      <Switch>
-        <Route exact path='/' component={Landing} />
-        <Route exact path='/login' component={Login} />
-        <PrivateRoute exact path='/dashboard' component={Dashboard} />
-      </Switch>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/login' component={Login} />
+          <PrivateRoute exact path='/dashboard' component={Dashboard} />
+          <PrivateRoute exact path='/marking' component={Marking} />
+        </Switch>
       </Suspense>
     </BrowserRouter>
   );
@@ -28,50 +30,52 @@ const Routes = () => {
 
 export default Routes;
 
-const PrivateRoute = ( { component: Component, ...rest }) => {
+const PrivateRoute = ({ component: Component, ...rest }) => {
   const [authState, setAuthState] = React.useState();
   const [user, setUser] = React.useState();
   const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
-      return onAuthUIStateChange((nextAuthState, authData) => {
-          setAuthState(nextAuthState);
-          setUser(authData)
-          setLoading(false)
-      });
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+      setLoading(false);
+    });
   }, []);
-
 
   return (
     <Route
       {...rest}
       render={props => {
-  
-        if (authState !== AuthState.SignedIn && !loading && !user ) {
+        if (authState !== AuthState.SignedIn && !loading && !user) {
           // not logged in so redirect to login page with the return url
-          return <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+          return (
+            <Redirect
+              to={{ pathname: '/login', state: { from: props.location } }}
+            />
+          );
         }
 
         // authorised so return component
-        return <Component {...props} />
+        return <Component {...props} />;
       }}
     />
-  )
+  );
 
-// return authState === AuthState.SignedIn && user ? (
-//     <div className="App">
-//         <div>Hello, {user.username}</div>
-//         <AmplifySignOut />
-//     </div>
-//   ) : (
-//     <AmplifyAuthenticator />
-// );
-}
+  // return authState === AuthState.SignedIn && user ? (
+  //     <div className="App">
+  //         <div>Hello, {user.username}</div>
+  //         <AmplifySignOut />
+  //     </div>
+  //   ) : (
+  //     <AmplifyAuthenticator />
+  // );
+};
 
-const useComponentWillMount = (cb) => {
-  const willMount = useRef(true)
+const useComponentWillMount = cb => {
+  const willMount = useRef(true);
 
-  if (willMount.current) cb()
+  if (willMount.current) cb();
 
-  willMount.current = false
-}
+  willMount.current = false;
+};
