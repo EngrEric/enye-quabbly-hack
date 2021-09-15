5268a6500cff435149c8944dfa3bb34a25e2a041
SantosBright <itzsantosbright15@gmail.com>
2021-09-15 13:13:23 +0100

    dashboard layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,44 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   future: {
-    removeDeprecatedGapUtilities: true
+    removeDeprecatedGapUtilities: true,
   },
   theme: {
     backgroundColor: theme => ({
       ...theme('colors'),
-      primary: '#2D9CDB'
+      primary: '#2D9CDB',
     }),
     extend: {
       fontFamily: {
         sans: ['Roboto', ...defaultTheme.fontFamily.sans],
-        manrope: 'Manrope'
+        manrope: 'Manrope',
+        lato: 'Lato',
+        montserrat: 'Montserrat',
       },
       fontSize: {},
       colors: {
-        primary: '#2D9CDB'
+        primary: '#2D9CDB',
+        neutral: '091E42',
       },
       screens: {
         sm: '640px',
         md: '768px',
         lg: '1024px',
         xl: '1280px',
-        xxl: '1408px'
+        xxl: '1408px',
       },
       borderColor: {
-        primary: '#2D9CDB'
+        primary: '#2D9CDB',
       },
       backgroundSize: {
         ...defaultTheme.backgroundSize,
-        'size-55': '55%'
+        'size-55': '55%',
       },
-      height: {}
-    }
+      height: {},
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
