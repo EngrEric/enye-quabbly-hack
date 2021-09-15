b77381f10091fe7f9dd27fd9c74a31390bc5e6af
SantosBright <itzsantosbright15@gmail.com>
2021-09-15 11:11:27 +0100

    add auth

--- src/dashboard/Index.js
+++ src/dashboard/Index.js
@@ -1,9 +1,14 @@
+import { AmplifySignOut } from '@aws-amplify/ui-react';
 import React from 'react'
 
 const Index = () => {
   return (
     <div>
       <h1>dashboard</h1>
+      <div className="container mx-auto">
+
+      <AmplifySignOut/>
+      </div>
     </div>
   )
 }
